d26dffec4f2c5fe4bab6d6583700465f2f6771a7
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2023-11-28 08:09:50 +0200

    final touch

--- src/components/certificate-component/certificate-component.jsx
+++ src/components/certificate-component/certificate-component.jsx
@@ -1,59 +1,123 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Input from '../input/input';
 
+const CertificateContainer = styled.div`
+  display: flex;
+  margin-top: 8px;
+  padding-right: 0;
+  flex-direction: column;
+`;
+
+const CertificateLabel = styled.label`
+  width: 100%;
+  margin-bottom: 8px;
+`;
+
+const CertificateButton = styled.div`
+  display: inline-block;
+  padding: 8px;
+  margin-right: 4px;
+  color: #28a745;
+  cursor: pointer;
+`;
+
+const CertificateRemoveButton = styled(CertificateButton)`
+  color: #dc3545;
+  margin-left: 4px;
+`;
+
+const CertificateRow = styled.div`
+  display: flex;
+  margin-top: 8px;
+`;
+
+const CertificateInput = styled(Input)`
+  margin-top: 8px;
+`;
+
+const CertificateTitle = styled.h2`
+  width: 95%;
+`;
+
+const CertificateTitleContainer = styled.div`
+  display: flex;
+`;
+
+const CertificateNumberContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 95%;
+`;
+
+const CertificateInputContainer = styled.div`
+width:100%;
+  ${({w50})=>w50 &&`width:50%`}
+`;
+
+const CertificateActionsContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
 const CertificateComponent = (props) => {
-   const [certificate, setCertificate]=useState([0]);
-    const renderCertifcate=async ()=>{
-        let ne = [...certificate,(certificate.length)]
-        await setCertificate(ne);
-        console.log(certificate);
+  const [certificate, setCertificate] = useState([0]);
 
-    }
-    const removeCertifcate=(i)=>{
-        let newCertificate=[...certificate];
-        newCertificate.splice(i,1);
-        setCertificate(newCertificate);
+  const renderCertificate = async () => {
+    let ne = [...certificate, certificate.length];
+    await setCertificate(ne);
+    console.log(certificate);
+  };
 
-    }
-    return (
+  const removeCertificate = (i) => {
+    let newCertificate = [...certificate];
+    newCertificate.splice(i, 1);
+    setCertificate(newCertificate);
+  };
+
+  return (
+    <CertificateContainer>
+      <CertificateTitleContainer>
+        <CertificateTitle>Certificate</CertificateTitle>
+        <CertificateButton onClick={renderCertificate}>+</CertificateButton>
+      </CertificateTitleContainer>
+
+      {certificate.length === 0 ? (
+        <></>
+      ) : (
         <>
-    <label className='col-11'>Certificate</label> {certificate.length == 0 ? <div className='col-1 btn btn-sm btn-outline-success' onClick={renderCertifcate}>+</div> :
-    <> <div className='col-1' ></div> {certificate.map((element,index)=>{return(<div key={element} className='row col-12 mt-2'>
-          <div className='row'>
-              <div className='col-10'></div>
-              <div className='col-1'>
-          <div className='btn btn-outline-success' onClick={renderCertifcate}>+</div>
-              </div>
-              <div className='col-1'>
-          <div className='btn btn-outline-danger' onClick={()=>{removeCertifcate(index)}}>-</div>
-              </div>
-          
-          </div>
-          <div className='col-12'>
-                  <label>
-                      number
-                  </label>
-              <Input type="text" />
-              </div>
-          <div className='col-6'>
-                  <label>
-                      Start at
-                  </label>
-              <Input type="date" />
-              </div>
-              <div className='col-6'>
-                  <label>
-                      End in
-                  </label>
-              <Input type="date" />
-              </div>
-                      
-                  </div>);})} </>
-}
-  
-        {/* <p>{certificate.toString()}</p> */}
+          <div></div>
+          {certificate.map((element, index) => {
+            return (
+              <CertificateRow key={element}>
+                <CertificateNumberContainer>
+                  <CertificateInputContainer>
+                    <CertificateLabel>Number</CertificateLabel>
+                    <CertificateInput type='text' />
+                  </CertificateInputContainer>
+                  <CertificateInputContainer w50={true}>
+                    <CertificateLabel>Start at</CertificateLabel>
+                    <CertificateInput type='date' />
+                  </CertificateInputContainer>
+                  <CertificateInputContainer w50={true}>
+                    <CertificateLabel>End in</CertificateLabel>
+                    <CertificateInput type='date' />
+                  </CertificateInputContainer>
+                  
+                </CertificateNumberContainer>
+
+                <CertificateActionsContainer>
+                  <CertificateButton onClick={renderCertificate}>+</CertificateButton>
+                  <CertificateRemoveButton onClick={() => removeCertificate(index)}>-</CertificateRemoveButton>
+                </CertificateActionsContainer>
+              </CertificateRow>
+            );
+          })}
         </>
-    );
-}
+      )}
+    </CertificateContainer>
+  );
+};
 
 export default CertificateComponent;
--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -3,7 +3,7 @@ import './input.css'
 
 const Input = (props) => {
     return (
-        <input type={props.type} className={"form-control mt-2 reg-input " +props.cl}  name={props.type}/>
+        <input type={props.type} className={"form-control mt-2 reg-input " +props.cl}  name={props.name}/>
     );
 }
 
